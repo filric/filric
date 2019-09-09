30ca3184f6e826a4d54deed1327fad4179215abf
filric <filippa.richardsson@elev.ga.ntig.se>
2019-09-09 13:16:56 +0200

    Klar med dagen

---
+++ V37/V37/lektion1.java
@@ -0,0 +1,25 @@
+package V37;
+
+public class lektion1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int heltal1 = 5; // skapar en låda som heter heltal1 och fyller den med ett heltal
+		int svar; //deklarera
+		
+		svar = heltal1*heltal1;
+		
+		System.out.println("Svar: " + svar);
+		
+		double pi = 3.14;
+		double radie = 5;
+		
+		double area = pi*radie*radie;
+				
+		System.out.println("Arean är: " + area + " cm^2");
+		
+	}
+
+}
---
+++ V37/V37/tryyourself.java
@@ -0,0 +1,22 @@
+package V37;
+
+public class tryyourself {
+
+	
+	public static void main(String[] args) {
+		
+		int heltal1 = 9;
+		int svar;
+		
+		svar = heltal1*heltal1;
+		
+		System.out.println("Svaret är: " + svar);
+		
+		double decimaltal = 12.5;
+		double radie = 0.5;
+		
+		double area  = heltal1*heltal1;
+				
+		System.out.println("Svaret är: " + area);
+	}
+}
