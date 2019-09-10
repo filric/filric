8ec4068f30f6ee1c9cf98c11209c906540a132a1
filric <filippa.richardsson@elev.ga.ntig.se>
2019-09-10 17:36:22 +0200

    Update hemuppgift.java

--- V37/V37/hemuppgift.java
+++ V37/V37/hemuppgift.java
@@ -9,9 +9,32 @@ public class hemuppgift {
 		
 		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Vad heter du?");
 		
+		String name1 = input.nextLine();
 		
+		System.out.println("Hur gammal ‰r du?");
+		
+		String name2 = input.nextLine();
 
+		System.out.println("Vad ‰r din adress?");
+		
+		String name3 = input.nextLine();
+		
+		System.out.println("Vad ‰r ditt postnummer?");
+		
+		String name4 = input.nextLine();
+		
+		System.out.println("Vilken stad bor du i?");
+		
+		String name5 = input.nextLine();
+		
+		System.out.println("Vad ‰r ditt telefonnummer?");
+		
+		String name6 = input.nextLine();
+		
+		System.out.println("\nInformation:");
+		System.out.println("\nNamn: \t\t" + name1 + "\n≈lder: \t\t" + name2 + "\nAdress: \t" + name3 + "\n \t\t" + name4 + "\t" + name5 + "\nTelefon: \t" + name6 );
 	}
 
 }
