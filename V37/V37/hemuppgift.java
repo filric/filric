7ca5fb51e745e1d424fe2522b9020e69a61b708c
filric <filippa.richardsson@elev.ga.ntig.se>
2019-09-10 12:13:51 +0200

    Update hemuppgift.java

--- V37/V37/hemuppgift.java
+++ V37/V37/hemuppgift.java
@@ -1,9 +1,16 @@
 package V37;
 
+import java.util.Scanner;
+
 public class hemuppgift {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		
 
 	}
 
