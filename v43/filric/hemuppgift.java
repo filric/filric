2b6937a20176dd0809db0b2783061787882d9cb6
filric <54800700+filric@users.noreply.github.com>
2019-11-03 14:15:04 +0100

    help

---
+++ v43/filric/hemuppgift.java
@@ -0,0 +1,125 @@
+package filric;
+
+import java.util.Scanner;
+
+public class hemuppgift {
+
+	public static void main(String[] args) {
+		
+		lol();
+		max();
+		count();
+		kelvinToCelsius();
+		fahrenheitToCelsius();
+		ageControl();
+		stair();
+		}
+ public static void lol() {
+	 System.out.println("AHAHAHAHHA");
+ }
+ public static int max(int a, int b) {
+	 
+	 Scanner input = new Scanner(System.in);
+	 a = input.nextInt();
+	 b = input.nextInt();
+	 
+	 if(a == b || a > b)
+		 
+		return a;
+	 
+	 else {
+		 return b;
+	 }
+ }
+ public static void count(int n) {
+	 
+	 Scanner input = new Scanner(System.in);
+	 
+	 System.out.println("Skriv in ett heltal");
+	 n = input.nextInt();
+	 
+	 int i = 1;
+	 
+	 while(i <= n) {
+		 System.out.println(i);
+		 i++;
+	 }
+	 
+ }
+ public static double kelvinToCelsius(double kelvin) {
+	 
+	 Scanner input = new Scanner(System.in);
+	 System.out.println("Skriv in en temperatur i Kelvin");
+	 kelvin = input.nextDouble();
+	 double celsius;
+	 
+	 if(kelvin > 0) {
+		 celsius = kelvin - 273.15;
+		 return celsius; 
+		 
+	 }
+	 else {
+		 System.out.println("Syntax error");
+		 return kelvin;
+	 }
+ }
+public static double fahrenheitToCelsius(double fahrenheit) {
+	 
+	 Scanner input = new Scanner(System.in);
+	 System.out.println("Skriv in en temperatur i Fahrenheit");
+	 fahrenheit = input.nextDouble();
+	 double celsius;
+	 
+	 celsius = (fahrenheit - 32)*5/9;
+	 return celsius; 
+		 
+ }
+public static void ageControl(int age) {
+	 
+	 Scanner input = new Scanner(System.in);
+	 
+	 System.out.println("Hur gammal är du?");
+	 age = input.nextInt();
+	 
+	 if(age < 6) {
+		 System.out.println("Småbarn får inte göra något");
+	 }
+	 else if(age < 13) {
+		 System.out.println("Du får spela Minecraft");
+	 }
+	 else if(age < 15) {
+		 System.out.println("Du är tonåring");
+	 }
+	 else if(age < 18) {
+		 System.out.println("Du får köra moppe");
+	 }
+	 else if(age < 21) {
+		 System.out.println("Du får köra bil");
+	 }
+	 else if(age < 65) {
+		 System.out.println("Du får vuxenstraff om du gör något dumt");
+	 }
+	 else if(age < 100) {
+		 System.out.println("Du är pensionär");
+	 }
+	 else {
+		 System.out.println("R.I.P?");
+	 }
+}
+public static void stair(int steps) {
+	
+	Scanner input = new Scanner(System.in);
+	 
+	 System.out.println("Antal steg?");
+	 steps = input.nextInt();
+	 
+	 int i = 1;
+	 String str = "x";
+	 
+	 while(i <= steps) {
+		 
+		 System.out.println(str);
+		 str = str + "x"; 
+	 } 
+}
+}
--- v43/filric/metoder.java
+++ v43/filric/metoder.java
@@ -5,36 +5,42 @@ import java.util.Scanner;
 public class metoder {
 
 	public static void main(String[] args) {
-		
-		int tal1;
+		twoNumbers();
+	}
+	/**
+	 * Skriver två heltal och skickar de vidare
+	 */
+	public static void twoNumbers(){
+	 
+	 int tal1;
 		int tal2;
-		
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Vänligen skriv in två heltal, med ett enter emellan");
-		
+
+		Scanner input  = new Scanner(System.in);
+
+		System.out.println("Vänligen skriv in två heltal: ");
 		tal1 = input.nextInt();
 		tal2 = input.nextInt();
 		
-		System.out.println(minstaTal(tal1,tal2));
-	}
-	/**
-	 * Jämför två heltal och utvärderar vilket som är minst. samt skickar tillbaka en hälsning.
-	 * @param tal1
-	 * @param tal2
-	 * @return String hej
-	 */
-	 static void minstaTal(int tal1, int tal2) {
-		if(tal1 <= tal2) {
+		System.out.println(minstaTal(tal1, tal2));
+ }
+/**
+ * Jämför två heltal och utvärderar vilket som är minst. Samt skickar
+ * tillbaka en hälsning.
+ * @param tal1 heltal1
+ * @param tal2 heltal2
+ * @return String hej
+ */
+	public static String minstaTal(int tal1, int tal2) {
+		
+		if (tal1 <= tal2) {
 			System.out.println("Minsta talet är: " + tal1);
-		}
-		else {
+		} else {
 			System.out.println("Minsta talet är: " + tal2);
 		}
 		
-		String hej = "Metoden är klar";
+		String hej = "Programmet är färdigt";
 		
 		return hej;
-	}
 
+	}
 }
