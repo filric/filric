7b2e2741b80c32736732c0affca25578365b741c
filric <54800700+filric@users.noreply.github.com>
2020-01-14 20:58:43 +0100

    Update bubbleSort.java

--- v.3/filric/bubbleSort.java
+++ v.3/filric/bubbleSort.java
@@ -1,32 +1,41 @@
 package filric;
 
+import java.util.Scanner;
 import java.util.Arrays;
 
 public class bubbleSort {
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("");
 
-		int[] numbers = { 7, 8, 4, 5, 1, 9, 2, 6, 3, 0 };
+		int[] numbers = new int[50];
 		boolean swapped;
 		int temp;
+		
+		for (int i = 0; i < numbers.length; i++) {//slumpar 50 heltal mellan 0-100
+			numbers[i] = (int) (Math.random()*100);
+		}
 
 		System.out.println(Arrays.toString(numbers));
-		
+
 		do {
-			
-			swapped = false;//nollställer swapped i början av jämnförelsen	
+
+			swapped = false;// nollställer swapped i början av jämnförelsen
 			for (int i = 0; i < numbers.length - 1; i++) {
 				if (numbers[i] > numbers[i + 1]) { // byter plats på två intilligande tal
 					temp = numbers[i]; // sparar tillfälligt värdet på vänstra sidan
 					numbers[i] = numbers[i + 1];
 					numbers[i + 1] = temp;
 					swapped = true;
-					System.out.println(Arrays.toString(numbers));
-				}
+									}
 			}
 
 		} while (swapped);
-
+		System.out.println(Arrays.toString(numbers));
+		
 		
 	}
 
